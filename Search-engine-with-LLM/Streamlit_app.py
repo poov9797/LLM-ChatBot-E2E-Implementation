9663fd13334d5981e8fd8e01d80df246a13d4322
poo9797 <poovarasan9797@gmail.com>
2024-08-15 11:34:13 +0100

    Search engine app

--- Search-engine-with-LLM/Streamlit_app.py
+++ Search-engine-with-LLM/Streamlit_app.py
@@ -11,6 +11,38 @@ client = OpenAI(
 
 es_client = Elasticsearch('http://localhost:9200') 
 
+# index_settings = {
+#     "settings": {
+#         "number_of_shards": 1,
+#         "number_of_replicas": 0
+#     },
+#     "mappings": {
+#         "properties": {
+#             "text": {"type": "text"},
+#             "section": {"type": "text"},
+#             "question": {"type": "text"},
+#             "course": {"type": "keyword"} 
+#         }
+#     }
+# }
+import requests 
+
+docs_url = 'https://github.com/DataTalksClub/llm-zoomcamp/blob/main/01-intro/documents.json?raw=1'
+docs_response = requests.get(docs_url)
+documents_raw = docs_response.json()
+
+documents = []
+
+for course in documents_raw:
+    course_name = course['course']
+
+    for doc in course['documents']:
+        doc['course'] = course_name
+        documents.append(doc)
+index_name = "course-questions"
+from tqdm.auto import tqdm
+for doc in tqdm(documents):
+    es_client.index(index=index_name, document=doc)
 
 def elastic_search(query, index_name = "course-questions"):
     search_query = {
@@ -75,6 +107,7 @@ def rag(query):
     search_results = elastic_search(query)
     prompt = build_prompt(query, search_results)
     answer = llm(prompt)
+
     return answer
 
 
@@ -86,6 +119,7 @@ def main():
     if st.button("Ask"):
         with st.spinner('Processing...'):
             output = rag(user_input)
+            # output="Success"
             st.success("Completed!")
             st.write(output)
 
